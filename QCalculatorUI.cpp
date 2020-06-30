851b4795a66fad2315f711013be028788774d6e2
LuYonglei <yonglei_lu@163.com>
2020-06-30 10:04:46 +0800

    处理非"="按钮按下,让文本框产生不同的动作

--- QCalculatorUI.cpp
+++ QCalculatorUI.cpp
@@ -32,7 +32,7 @@ bool QCalculatorUI::construct(void)
     my_edit = new QLineEdit(this);
     if(my_edit != nullptr){                           //内存申请成功
         my_edit->setReadOnly(true);
-        my_edit->setAlignment(Qt::AlignRight);
+        my_edit->setAlignment(Qt::AlignRight);        //设置文本内容右对齐
         my_edit->move(10, 10);                        //文本框起始位置(10,10)
         my_edit->resize(240, 30);                     //文本框长240,高30
     }else{
@@ -73,6 +73,7 @@ void QCalculatorUI::show(void)
 {
     QWidget::show();                               //调用父类函数,显示主窗口
     setFixedSize(width(),height());                //设置主窗口固定大小,只有在show()之后,调用height()和weight()获得的结果才是准确的
+                                                   //原因在于不同的操作系统的边框,标题框,宽\高可能不同
 }
 
 QCalculatorUI::~QCalculatorUI(void){
@@ -82,5 +83,22 @@ QCalculatorUI::~QCalculatorUI(void){
 void QCalculatorUI::onButtonClicked(void)
 {
     QPushButton * btn = static_cast<QPushButton *>(sender());//sender()隶属于QObject,返回指针,该指针指向信号发送者
+    QString clickedText = btn->text();                       //获取当前用户输入的字符
+    QString lineText = my_edit->text();                      //获取文本框当前内容
+    if(clickedText == "<-"){
+        if (lineText.length() > 0){                          //如果文本框当前存在字符
+            lineText.remove(lineText.length()-1,1);          //末尾删除一个字符
+            my_edit->setText(lineText);                      //字符回显
+        }
+    }else if(clickedText == "C"){                            //清空文本框中的字符
+        my_edit->clear();
+    }else if(clickedText == "="){
+        if (lineText.length() > 0){
+
+        }
+    }else{
+        lineText += clickedText;                             //将文本框内容和按钮内容组合
+        my_edit->setText(lineText);                          //在文本框显示组合结果
+    }
     qDebug()<<btn->text();
 }
